8e81e9885cd2b2f9ca02ce976651dd8d0cb02f2a
IlhamLamp <ilhamutomo24@gmail.com>
2023-05-19 03:03:03 +0700

    add: textStyle

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-Color whiteColor = Color(0xffffff);
+const Color whiteColor = Color(0xffffff);
+const Color primaryColor = Color(0x8E44AD);
+const Color secondaryColor = Color(0x433847);
+const Color formTextColor = Color(0x2C2929);
+
+TextStyle whiteTextStyle = GoogleFonts.dmSans(
+  color: whiteColor,
+);
+TextStyle primaryTextStyle = GoogleFonts.dmSans(
+  color: primaryColor,
+);
+TextStyle secondaryTextStyle = GoogleFonts.dmSans(
+  color: secondaryColor,
+);
+TextStyle formTextStyle = GoogleFonts.dmSans(
+  color: formTextColor,
+);
